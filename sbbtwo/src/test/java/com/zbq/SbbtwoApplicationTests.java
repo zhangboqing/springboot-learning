7736c7d89662b908aaff45a2cfc3ffec41278202
zhangboqing <zhangboqing@beiquan.org>
2017-07-21 18:25:31 +0800

    sping boot @Scheduled定时任务、@Async异步调用

---
+++ sbbtwo/src/test/java/com/zbq/SbbtwoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zbq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbbtwoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
